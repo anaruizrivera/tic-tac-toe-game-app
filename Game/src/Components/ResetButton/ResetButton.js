bffa55daa321a230f3472c10e359bd095c80b39e
Darwin Bueso <darwingaldamez5@gmail.com>
2020-05-27 11:33:08 -0400

    PushingGame

---
+++ Game/src/Components/ResetButton/ResetButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { tsPropertySignature } from '@babel/types';
+
+const ResetButton = (props) => {
+    return (
+        <>
+            <button className="reset--button" onClick={props.onClick}>
+                Reset Game!
+            </button>
+        </>
+    )
+}
+
+export default ResetButton;
